6020688f41f1a22a754ae3f550ad3345dd29219f
나윤수 <skennel2@gmail.com>
2018-06-11 12:29:46 +0900

    add constructor

--- jpa/src/main/java/org/almansa/app/service/exception/OrderException.java
+++ jpa/src/main/java/org/almansa/app/service/exception/OrderException.java
@@ -1,10 +1,16 @@
 package org.almansa.app.service.exception;
 
+import org.almansa.app.domain.exception.RemainingStockException;
+
 public class OrderException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
-    public OrderException(String message) {
-        super(message);
+    public OrderException(RemainingStockException e) {
+        super(e);
+    }
+    
+    public OrderException(RemainingStockException e, String message) {
+        super(message, e);
     }
 }
